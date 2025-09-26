f91fda50123459dfeeb2364ca12ff7500d00a292
Iria <iriairis51@gmail.com>
2025-09-26 08:49:09 +0200

    Preparation data done

---
+++ data_preparation.py
@@ -0,0 +1,128 @@
+import pandas as pd
+
+def prepare_airline_data(airlines_data):
+    airlines_data = airlines_data.rename(
+        columns={
+            'IATA_CODE' : 'airline_iata',
+            'AIRLINE' : 'airline_name'
+        }
+    )
+    return airlines_data[['airline_iata', 'airline_name']].drop_duplicates()
+
+def prepare_airport_data(airports_data):
+    airports_data = airports_data.rename(
+        columns={
+            'IATA_CODE' : 'iata_code',
+            'AIRPORT' : 'airport_name',
+            'CITY' : 'city',
+            'STATE' : 'state',
+            'COUNTRY' : 'country',
+            'LATITUDE' : 'latitude',
+            'LONGITUDE' : 'longitude'
+        }
+    )
+    return airports_data[['iata_code', 'airport_name', 'city', 'state', 'country', 'latitude', 'longitude']].drop_duplicates()
+
+def prepare_date_data(flights_data):
+    new_date_data = flights_data[['YEAR', 'MONTH', 'DAY', 'DAY_OF_WEEK']].drop_duplicates().copy()
+    new_date_data = new_date_data.rename(
+        columns={
+            'YEAR' : 'year',
+            'MONTH' : 'month',
+            'DAY' : 'day',
+            'DAY_OF_WEEK' : 'day_of_week'
+        }
+    )
+    days = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'}
+    new_date_data['day_of_week'] = new_date_data['day_of_week'].map(days)
+    return new_date_data
+
+def prepare_cancellation_data(flights_data):
+    new_cancellation_data = flights_data[['CANCELLATION_REASON']].drop_duplicates().dropna().copy()
+    new_cancellation_data = new_cancellation_data.rename(
+        columns={
+            'CANCELLATION_REASON' : 'cancellation_type'
+        }
+    )
+
+    return new_cancellation_data
+
+def prepare_flight_data(flight_data, engine):
+    
+    #Change time values to proper format
+    time_columns = ['SCHEDULED_DEPARTURE', 'DEPARTURE_TIME', 'WHEELS_OFF', 
+                    'WHEELS_ON', 'SCHEDULED_ARRIVAL', 'ARRIVAL_TIME']
+    for col in time_columns:
+        flight_data[col] = pd.to_datetime(flight_data[col], format = '%H%M', errors='coerce').dt.strftime
+    
+    cancellation_db = pd.read_sql('SELECT cancellation_id, cancellation_type FROM cancellation_reason', con=engine)
+
+    new_flight_data = flight_data.merge(
+        cancellation_db,
+        how='left',
+        left_on='CANCELLATION_REASON',
+        right_on='cancellation_type'
+    ).drop(columns=['CANCELLATION_REASON', 'cancellation_type'])
+
+    date_db = pd.read_sql('SELECT date_id, year, month, day FROM date', con=engine)
+    new_flight_data = new_flight_data.merge(
+        date_db,
+        how='left',
+        left_on=['YEAR', 'MONTH', 'DAY'],
+        right_on=['year', 'month', 'day']
+    ).drop(columns=['YEAR', 'MONTH', 'DAY', 'year', 'month', 'day'])
+
+    airport_db = pd.read_sql('SELECT airport_id, iata_code FROM airport', con=engine)
+    new_flight_data = new_flight_data.merge(
+        airport_db,
+        how='left',
+        left_on='ORIGIN_AIRPORT',
+        right_on='iata_code'
+    ).drop(columns=['iata_code', 'ORIGIN_AIRPORT'])
+
+    new_flight_data= new_flight_data.rename(columns={'airport_id': 'origin_airport_id'})
+
+    new_flight_data = new_flight_data.merge(
+        airport_db,
+        how='left',
+        left_on='DESTINATION_AIRPORT',
+        right_on='iata_code',
+    ).drop(columns=['iata_code', 'DESTINATION_AIRPORT'])
+    new_flight_data= new_flight_data.rename(columns={'airport_id': 'destination_airport_id'})
+
+    airline_db = pd.read_sql('SELECT airline_iata, airline_id FROM airline', con=engine)
+    new_flight_data = new_flight_data.merge(
+        airline_db,
+        how='left',
+        left_on='AIRLINE',
+        right_on='airline_iata'
+    ).drop(columns=['AIRLINE', 'airline_iata'])
+
+    new_flight_data = new_flight_data.rename(
+        columns = {
+            'FLIGHT_NUMBER': 'flight_number',
+            'TAIL_NUMBER': 'aircraft_id',
+            'SCHEDULED_DEPARTURE': 'scheduled_departure',
+            'SCHEDULED_TIME': 'scheduled_time',
+            'DEPARTURE_TIME': 'departure_time',
+            'DEPARTURE_DELAY': 'departure_delay',
+            'TAXI_OUT': 'taxi_out',
+            'WHEELS_OFF': 'wheels_off',
+            'ELAPSED_TIME': 'elapsed_time',
+            'AIR_TIME': 'air_time',
+            'DISTANCE': 'distance',
+            'WHEELS_ON': 'wheels_on',
+            'TAXI_IN': 'taxi_in',
+            'SCHEDULED_ARRIVAL': 'scheduled_arrival',
+            'ARRIVAL_TIME':'arrival_time',
+            'ARRIVAL_DELAY':'arrival_delay',
+            'DIVERTED': 'is_diverted',
+            'CANCELLED':'is_cancelled'
+        }
+    )
+
+    needed_columns = ['flight_number','aircraft_id', 'airline_id', 'origin_airport_id', 'destination_airport_id', 'date_id', 'cancellation_id',
+                      'scheduled_departure', 'scheduled_time', 'departure_time', 'departure_delay', 'taxi_out', 'wheels_off', 'elapsed_time', 
+                      'air_time', 'distance', 'wheels_on', 'taxi_in', 'scheduled_arrival', 'arrival_time', 'arrival_delay', 'is_diverted', 'is_cancelled']
+
+    return new_flight_data[needed_columns]
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from data_preparation import *
 import json
 
 def load_db_config(config_file='config.json'):
@@ -10,98 +11,6 @@ def get_connection(db_config):
     return create_engine(
         f'mysql+pymysql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}'
     )
-
-
-def prepare_airline_data(airlines_data):
-    new_airlines_data = airlines_data[['IATA_CODE', 'AIRLINE']].drop_duplicates().copy()
-    new_airlines_data = new_airlines_data.rename(
-        columns={
-            'IATA_CODE' : 'airline_iata',
-            'AIRLINE' : 'airline_name'
-        }
-    )
-    return new_airlines_data
-
-def prepare_airport_data(airports_data):
-    new_airports_data = airports_data[['IATA_CODE', 'AIRPORT', 'CITY', 'STATE', 'COUNTRY', 'LATITUDE', 'LONGITUDE']].drop_duplicates().copy()
-    new_airports_data = new_airports_data.rename(
-        columns={
-            'IATA_CODE' : 'iata_code',
-            'AIRPORT' : 'airport_name',
-            'CITY' : 'city',
-            'STATE' : 'state',
-            'COUNTRY' : 'country',
-            'LATITUDE' : 'latitude',
-            'LONGITUDE' : 'longitude'
-        }
-    )
-    return new_airports_data
-
-def prepare_date_data(flights_data):
-    new_date_data = flights_data[['YEAR', 'MONTH', 'DAY', 'DAY_OF_WEEK']].drop_duplicates().copy()
-    new_date_data = new_date_data.rename(
-        columns={
-            'YEAR' : 'year',
-            'MONTH' : 'month',
-            'DAY' : 'day',
-            'DAY_OF_WEEK' : 'day_of_week'
-        }
-    )
-    days = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'}
-    new_date_data['day_of_week'] = new_date_data['day_of_week'].map(days)
-    return new_date_data
-
-def prepare_cancellation_data(flights_data):
-    new_cancellation_data = flights_data[['CANCELLATION_REASON']].drop_duplicates().dropna().copy()
-    new_cancellation_data = new_cancellation_data.rename(
-        columns={
-            'CANCELLATION_REASON' : 'cancellation_type'
-        }
-    )
-
-    return new_cancellation_data
-
-def prepare_flight_data(flights_data):
-    #TODO: No copiar. Trabajar sobre la data inicial y luego seleccionar las columnas necesarias
-    #TODO: lidiar con nulos
-    new_flight_data = flights_data[['FLIGHT_NUMBER', 'TAIL_NUMBER', 'ORIGIN_AIRPORT', 'DESTINATION_AIRPORT', 'SCHEDULED_DEPARTURE', 'DEPARTURE_TIME', 'DEPARTURE_DELAY', 'TAXI_OUT', 'WHEELS_OFF', 'SCHEDULED_TIME', 'ELAPSED_TIME', 'AIR_TIME', 'DISTANCE', 'WHEELS_ON', 'TAXI_IN', 'SCHEDULED_ARRIVAL', 'ARRIVAL_TIME', 'ARRIVAL_DELAY', 'DIVERTED', 'CANCELLED', 'CANCELLATION_CODE']].copy()
-    new_flight_data['SCHEDULED_DEPARTURE'] = pd.to_datetime(new_flight_data['SCHEDULED_DEPARTURE'], format='%H%M').dt.time
-    new_flight_data['DEPARTURE_TIME'] = pd.to_datetime(new_flight_data['DEPARTURE_TIME'], format='%H%M').dt.time
-    new_flight_data['DEPARTURE_DELAY'] = pd.to_datetime(new_flight_data['DEPARTURE_DELAY'], format='%H%M').dt.time
-    new_flight_data['SCHEDULED_TIME'] = pd.to_datetime(new_flight_data['SCHEDULED_TIME'], format='%H%M').dt.time
-    new_flight_data['WHEELS_OFF'] = pd.to_datetime(new_flight_data['WHEELS_OFF'], format='%H%M').dt.time
-    new_flight_data['WHEELS_ON'] = pd.to_datetime(new_flight_data['WHEELS_ON'], format='%H%M').dt.time
-    new_flight_data['SCHEDULED_ARRIVAL'] = pd.to_datetime(new_flight_data['SCHEDULED_ARRIVAL'], format='%H%M').dt.time
-    new_flight_data['ARRIVAL_TIME'] = pd.to_datetime(new_flight_data['ARRIVAL_TIME'], format='%H%M').dt.time
-    
-    new_flight_data = new_flight_data.rename(
-        columns={
-            'AIRLINE' : 'airline_id',
-            'FLIGHT_NUMBER' : 'flight_id',
-            'TAIL_NUMBER' : 'aircraft_id',
-            'ORIGIN_AIRPORT' : 'origin_airport_id',
-            'DESTINATION_AIRPORT' : 'destination_airport_id',
-            'SCHEDULED_DEPARTURE' : 'scheduled_departure',
-            'DEPARTURE_TIME' : 'departure_time',
-            'DEPARTURE_DELAY' : 'departure_delay',
-            'TAXI_OUT' : 'taxi_out',
-            'WHEELS_OFF' : 'wheels_off',
-            'SCHEDULED_TIME' : 'scheduled_time',
-            'ELAPSED_TIME' : 'elapsed_time',
-            'AIR_TIME' : 'air_time',
-            'DISTANCE' : 'distance',
-            'WHEELS_ON' : 'wheels_on',
-            'TAXI_IN' : 'taxi_in',
-            'SCHEDULED_ARRIVAL' : 'scheduled_arrival',
-            'ARRIVAL_TIME' : 'arrival_time',
-            'ARRIVAL_DELAY' : 'arrival_delay',
-            'DIVERTED' : 'diverted',
-            'CANCELLED' : 'cancelled',
-            'CANCELLATION_CODE' : 'cancellation_code'
-        }
-    )
-    
-    return new_flight_data
 
 
 def load_airline_data(engine, airlines_data):
@@ -136,6 +45,14 @@ def load_cancellation_data(engine, cancellation_data):
     else:
         print("Cancellation dimension table loaded successfully.")
 
+def load_flight_data(engine, flight_data):
+    try:
+        flight_data.to_sql('fact_flights', con=engine, if_exists='append', index=False)
+    except Exception as ex:
+        print("Error while loading flight fact table: \n", ex)
+    else:
+        print("Flight fact table loaded successfully.")
+
 if __name__ == '__main__':
     
     airlines_data = pd.read_csv("Data/airlines.csv")
@@ -155,3 +72,4 @@ if __name__ == '__main__':
     load_airport_data(engine, prepare_airport_data(airports_data))
     load_date_data(engine, prepare_date_data(flights_data))
     load_cancellation_data(engine, prepare_cancellation_data(flights_data))
+    load_flight_data(engine,prepare_flight_data(flights_data, engine))
